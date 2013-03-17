e8d090727ef787aa5037d3d0045186bc73e1199d
iainrobertson <iain_git@jirks.net>
2013-03-17 15:22:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhraseApp::Application.config.secret_token = '1bed1e9d3f7fc7f08a5bac6147dfbe10eace607092d11f6f27efc2fd68ba02136d7ff3b05f2f878639874c8e9325d5d8d6574c62fb83d59d5b12e1d19f1cb7b0'
