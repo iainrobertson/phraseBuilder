25498fbd317d9c8f1d8dd3808ef97ec370645b26
iainrobertson <iain_git@jirks.net>
2013-03-20 15:18:35 +0000

    Finish Layout & routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,62 +1,34 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
-
-    it "should have the h1 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
-    end
-
-    it "should have the title 'Phrase Builder'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => 'Phrase Builder')
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
-
+    before { visit root_path }
+	it { should have_selector('h1',    text: 'Phrase Builder') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Phrase Builder'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Phrase Builder")
-    end
-
-    it "should have a custom page title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => '| Help')
-    end
-
+    before { visit help_path }
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) } 
   end
 
   describe "About page" do
-
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'Phrase Builder'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Phrase Builder")
-    end
-
-    it "should have a custom page title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => '| About Us')
-    end
-
+    before { visit about_path }
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About')) } 
   end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) } 
+  end
+
+
 end
