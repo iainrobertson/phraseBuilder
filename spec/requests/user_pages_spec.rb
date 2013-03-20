caac0b4574f756f7c02ff0e1cd661490b2bd3c86
iainrobertson <iain_git@jirks.net>
2013-03-20 21:04:04 +0000

    user authentication code

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,7 @@ describe "UserPages" do
 
   subject { page }
 
-  describe "signup page" do
+  describe "Signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
